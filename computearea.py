e2359ff8b6468351136b591de3cd9e7e822a07b3
Jessica <42187728+jhoneal@users.noreply.github.com>
2018-11-07 17:18:31 -0500

    Create computearea.py

---
+++ computearea.py
@@ -0,0 +1,10 @@
+# assign a radius
+radius = 20.0
+
+# compute area
+area = radius * radius * 3.14159
+
+# display results
+
+
+print("The area for the circle of radius {} is {}".format(radius,area))
