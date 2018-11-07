f42a663413c068db2fd9d0af22cb976cb817f816
Jessica <42187728+jhoneal@users.noreply.github.com>
2018-11-07 16:33:49 -0500

    Create agein5.py

---
+++ agein5.py
@@ -0,0 +1,14 @@
+
+
+def main():
+	
+	
+	
+	name = input("What is your name?")
+	print("Hi,",name)
+	
+	age = int(input("How old are you?"))
+	
+	print("Did you know that in five years you will be {} years old? And five years ago you were {}! Imagine that!".format(age+5, age-5))
+	
+main()
