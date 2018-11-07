b9f1df4eb1a06397feb468bf362bc9faa89aea64
Jessica <42187728+jhoneal@users.noreply.github.com>
2018-11-07 16:41:28 -0500

    Create AskingQuestions.py

---
+++ AskingQuestions.py
@@ -0,0 +1,18 @@
+
+
+def main():
+	
+	
+	print("Hello. What's your name?")
+	name = input()
+	
+	print("Hi,"+ name+ "! How old are you?")
+	age = input()
+	
+	print("So you're", age, "eh? That's not old at all! How much do you make,"+ name + "?")
+	wage = input()
+	
+	print(wage+"! I hope that's per hour and not per year!")
+	
+	
+main()
