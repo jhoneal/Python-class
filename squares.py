63578ebf91046e042ddec57cf0bff15619fcb79e
Jessica <42187728+jhoneal@users.noreply.github.com>
2018-11-14 10:11:56 -0500

    Create squares.py

---
+++ squares.py
@@ -0,0 +1,9 @@
+"""3. Squares
+
+Write a program to display the square of every integer upto a specified integer."""
+
+
+num = int(input("Enter a number: "))
+
+for i in range(num+1):
+	print(i**2)
